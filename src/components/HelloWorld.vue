c90ae7047b8b6b502fb029556d38b31daf57452c
张航 <zz_dream_fly@163.com>
2023-03-31 15:47:38 +0800

    feat: test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
 const count = ref(0)
 </script>
 
@@ -10,14 +9,14 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+     <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+        >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
