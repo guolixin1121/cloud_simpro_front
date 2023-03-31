d6394f8afc49a78e68c1395605d785dab6fe8914
张航 <zz_dream_fly@163.com>
2023-03-31 16:11:00 +0800

    fix: test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,7 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-
-
-     <code>components/HelloWorld.vue</code> t1111o test HMR
+     <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
