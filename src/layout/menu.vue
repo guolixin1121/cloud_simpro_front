ef991bf3c446d343e6a7828e35f0e53ab55b0246
zz_dream_fly@163.com <zz_dream_fly@163.com>
2023-04-07 09:54:10 +0800

    fix(menu.vue): 代码格式化
    代码格式化

--- src/layout/menu.vue
+++ src/layout/menu.vue
@@ -1,31 +1,33 @@
 <!-- eslint-disable no-undef -->
 <script setup lang="ts">
-import * as Icons from '@ant-design/icons-vue';
+import * as Icons from '@ant-design/icons-vue'
 
 const props = defineProps({
-    menus: {
-      type: Array<MenuItem>,
-      required: true
-    }
-  })
+  menus: {
+    type: Array<MenuItem>,
+    required: true
+  }
+})
 </script>
 
 <template>
-    <template v-for="menu in props.menus">
-      <template v-if="!menu.children || !menu.children.length">
-        <a-menu-item :key="menu.path">
-          <component :is="Icons[(menu.icon || 'UserOutlined') as keyof typeof Icons]"></component>
-          <span><router-link :to="menu.path">{{ menu.title }}</router-link></span>
-        </a-menu-item>
-      </template>
-      <a-sub-menu v-else :key="menu.path">
-        <template #title>
-          <component :is="Icons[(menu.icon || 'UserOutlined') as keyof typeof Icons]"></component>
-          <span>{{menu.title}}</span>
-        </template>
-        <Menu :menus="menu.children" />
-      </a-sub-menu>
+  <template v-for="menu in props.menus">
+    <template v-if="!menu.children || !menu.children.length">
+      <a-menu-item :key="menu.path">
+        <component :is="Icons[(menu.icon || 'UserOutlined') as keyof typeof Icons]"></component>
+        <span
+          ><router-link :to="menu.path">{{ menu.title }}</router-link></span
+        >
+      </a-menu-item>
     </template>
+    <a-sub-menu v-else :key="menu.path">
+      <template #title>
+        <component :is="Icons[(menu.icon || 'UserOutlined') as keyof typeof Icons]"></component>
+        <span>{{ menu.title }}</span>
+      </template>
+      <Menu :menus="menu.children" />
+    </a-sub-menu>
+  </template>
 </template>
 
 <style scoped></style>
