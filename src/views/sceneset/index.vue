9f8c1abe2be11678e8c3e5e921d396fd0429c4cc
郭丽新 <guolixin@saimo.ai>
2023-05-12 18:08:38 +0800

    feat: update sceneset table

--- src/views/sceneset/index.vue
+++ src/views/sceneset/index.vue
@@ -1,27 +1,60 @@
 <template>
   <search-form :items="formItems" @on-search="onSearch"></search-form>
 
-  <div class="main">
+  <div class="main" style="height: calc(100% - 100px)">
     <div class="flex justify-between items-center">
       <span class="title">场景集管理</span>
       <a-button type="primary" v-if="user.hasPermission('add')" @click="router.push('/sceneset/edit/0')">创建场景集</a-button>
     </div>
-
-    <Table :api="scenesetApi" :query="query" :columns="columns" :scroll="{ y: 'auto' }" :pagination="false">
-    </Table>
+   
+    <div style="height: calc(100% - 50px);" class="mt-4 overflow-auto">
+      <vxe-table
+        stripe
+        height="100%"
+        :row-config="{isHover: true}"
+        :tree-config="{transform: true}"
+        :expand-config="{ expandAll: true }"
+        :loading="loading"
+        :data="tableData">
+        <vxe-column
+          v-for="column in columns"
+          :key="column.dataIndex"
+          :field="column.dataIndex"
+          :title="column.title"
+          :width="column.width"
+          :tree-node="column.dataIndex === 'name'">
+        </vxe-column>
+        <vxe-column title="操作" width="150">
+          <template #default="{ row }">
+            <a class="text-blue mr-2" v-if="row.isLeaf === 1" @click="onView(row)">查看</a>
+            <a class="text-blue mr-2" v-if="row.isLeaf === 1" @click="onEdit(row)">编辑</a>
+            <a-popconfirm
+              title="你确定要删除吗？"
+              ok-text="是"
+              cancel-text="否"
+              @confirm="onDelete(row)"
+            >
+              <a class="text-blue mr-2">删除</a>
+            </a-popconfirm>
+          </template>
+        </vxe-column>
+      </vxe-table>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import 'vxe-table/lib/style.css'
+import { VxeTable, VxeColumn } from 'vxe-table'
+import { isEmpty } from '@/utils/tools'
+
 /****** api */
 const user = store.user
 const currentApi = api.scenesets
-const scenesetApi = (args: object) => currentApi.getList({ tree: 1, ...args })
+const scenesetApi = (args: object) => currentApi.getList({tree: 1, ...args })
 const tagsApi = (args: object) => api.tags.getList({ tag_type: 2, ...args })
 
 /****** 搜素区域 */
-type Query = Record<string, any>
-const query: Query = ref({})
 const formItems = ref<SearchFormItem[]>([
   { label: '名称', key: 'name', type: 'input', placeholder: '请输入场景集名称' },
   {
@@ -34,32 +67,54 @@ const formItems = ref<SearchFormItem[]>([
     defaultValue: ['']
   }
 ])
-const onSearch = (data: Query) => (query.value = data)
+const onSearch = (query: RObject) => fetchTableData(query)
 
 /****** 表格区域 */
 const router = useRouter()
 const columns = [
-  { title: '场景集名称', dataIndex: 'name', width: 200, ellipsis: true },
-  // { title: '标签', dataIndex: 'labels_detail', ellipsis: true },
-  { title: '路径', dataIndex: 'path', width: 200, ellipsis: true },
-  { title: '场景数量', dataIndex: 'count', width: 100 },
-  // { title: '创建时间', dataIndex: 'create_time', width: 180 },
-  {
-    title: '操作',
-    dataIndex: 'actions',
-    fixed: 'right',
-    width: 100,
-    actions: {
-      查看: {
-        validate: (data: RObject) => data.isLeaf === 1,
-        handler: (data: any) => router.push('/sceneset/view/' + data.id)
-      },
-      编辑: {
-        validate: (data: RObject) => data.isLeaf === 1,
-        handler: (data: any) => router.push('/sceneset/edit/' + data.id),
-      },
-      删除: async ({ id }: { id: string }) => await currentApi.delete(id)
-    }
-  }
+  { title: '场景集名称', dataIndex: 'name', ellipsis: true },
+  { title: '路径', dataIndex: 'path', ellipsis: true },
+  { title: '场景数量', dataIndex: 'count', width: 150 }
 ]
+
+const onDelete = async (row: RObject) => {
+  console.log(row)
+   await currentApi.delete(row.id)
+   fetchTableData()
+}
+const onView = ({id} : RObject) => router.push('/sceneset/view/' + id)
+const onEdit = ({id} : RObject) => router.push('/sceneset/edit/' + id)
+
+const loading = ref(false)
+const tableData = ref([])
+const fetchTableData = async (params: any = {}) => {
+  loading.value = true
+  const res = await scenesetApi(params)
+  tableData.value = transformTreeToArray(res.results)
+  loading.value = false
+}
+const transformTreeToArray = (data: []) => {
+  let results: any = []
+  if(!isEmpty(data)) {
+    data.forEach((item: RObject) => {
+      const itemObj = {...item}
+      const children = transformTreeToArray(item.children)
+      delete itemObj.children
+      results.push(item)
+      results.push(...children)
+    })
+  }
+  return results
+}
+fetchTableData()
 </script>
+
+<style scoped>
+:deep(.vxe-table--render-default) .vxe-body--row.row--stripe {
+  background: #f7f8fa;
+}
+:deep(.vxe-table--render-default) .vxe-cell--title {
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.85)
+}
+</style>
