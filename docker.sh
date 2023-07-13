510ea46e439e0b8289d84269c4e4d1e620a4387c
zz_dream_fly@163.com <zz_dream_fly@163.com>
2023-07-13 13:32:41 +0800

    fix(仿真结果a列表 取'display_name'字段 替换name): 仿真结果a列表 取'display_name'字段 替换name

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-export VERSION='0.95'
+export VERSION='0.96'
 
 docker build -t cloud_simpro_front .
 # docker save cloud_simpro_front > cloud_simpro_front.tar
--- src/views/simpro-result/components/scene.vue
+++ src/views/simpro-result/components/scene.vue
@@ -1,18 +1,16 @@
 <template>
   <a-spin :spinning="loading">
-    <Table
-      ref="tableRef" 
-      :api="getScenes"
-      :columns="columns"
-      :scroll="{ y: 'auto' }">
-      <template #bodyCell="{column, record}">
+    <Table ref="tableRef" :api="getScenes" :columns="columns" :scroll="{ y: 'auto' }">
+      <template #bodyCell="{ column, record }">
         <template v-if="column.dataIndex == 'actions'">
-          <a class="text-blue mr-2"
-            v-if="record.b_file && record.baidu_id" 
-            @click="replay(record)">回放</a>
-          <a class="text-blue"
+          <a class="text-blue mr-2" v-if="record.b_file && record.baidu_id" @click="replay(record)">回放</a>
+          <a
+            class="text-blue"
             v-if="record.obs_report && record.baidu_id"
-            :href="'/api/simpro/resource/' + record.obs_report" target="_blank" >查看报告</a>
+            :href="'/api/simpro/resource/' + record.obs_report"
+            target="_blank"
+            >查看报告</a
+          >
         </template>
       </template>
     </Table>
@@ -20,28 +18,30 @@
 </template>
 
 <script setup lang="ts">
-import { gotoVnc } from '@/utils/vnc';
+import { gotoVnc } from '@/utils/vnc'
 const task = useRoute().params.id
 const getScenes = () => api.result.getScenes({ task })
 
 const columns = [
-  { dataIndex: 'scene_id', title: '场景ID', width: 150},
-  { dataIndex: 'adsName', title: '场景名称'},
-  { dataIndex: 'labels_detail', title: '场景标签'},
-  { dataIndex: 'batch', title: '仿真轮次', width: 100},
+  { dataIndex: 'scene_id', title: '场景ID', width: 150 },
+  { dataIndex: 'adsName', title: '场景名称' },
+  { dataIndex: 'labels_detail', title: '场景标签', apiField: 'display_name' },
+  { dataIndex: 'batch', title: '仿真轮次', width: 100 },
   {
-    dataIndex: 'actions', title: '操作', fixed: 'right', width: 150
+    dataIndex: 'actions',
+    title: '操作',
+    fixed: 'right',
+    width: 150
   }
 ]
-
 const loading = ref(false)
 const uuid = useRoute().query.u
 const replay = (record: any) => {
   gotoVnc(
-    { 
+    {
       action: 3,
       value: JSON.stringify({ uuid: uuid, baidu_id: record.baidu_id })
-    }, 
+    },
     loading
   )
 }
