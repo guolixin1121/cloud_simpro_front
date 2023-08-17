2cf964f829535fbb24afcf2c9060e1eaaec9299e
Lixin <guolixin1121@126.com>
2023-08-17 10:05:29 +0800

    feat: update login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,27 +1,126 @@
 <template>
-  <a-form style="margin: 0 auto" class="pt-20 w-1/2">
-    <a-form-item label="用户名">
-      <a-input v-model:value="username"></a-input>
-    </a-form-item>
-    <a-form-item label="密码">
-      <a-input-password v-model:value="password"></a-input-password>
-    </a-form-item>
-    <a-form-item>
-      <a-button @click="login">登录</a-button>
-    </a-form-item>
-  </a-form>
+  <div class="login-container">
+    <span class="pro-name">智驾云平台</span>
+    <a-form style="margin: 0 auto" @finish="login" :model="formData" autocomplete="off">
+      <span class="title">登录</span>
+      <label className="label">账户</label>
+      <a-form-item name="username" :rules="[{ required: true, message: '请输入账户' }]" autocomplete="off">
+        <a-input name="username" v-model:value="formData.username" placeholder="请输入账户" autocomplete="off"></a-input>
+      </a-form-item>
+      <label className="label">密码</label>
+      <a-form-item name="password" :rules="[{ required: true, message: '请输入密码' }]" autocomplete="off">
+        <a-input-password
+          name="password"
+          placeholder="请输入密码"
+          v-model:value="formData.password"
+          autocomplete="off"
+        ></a-input-password>
+      </a-form-item>
+      <a-checkbox v-model:checked="checked" @change="handleChange">记住密码</a-checkbox>
+      <a-form-item>
+        <a-button class="login-btn" type="primary" html-type="submit" :loading="loading">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
 <script setup lang="ts">
-const username = ref('admin')
-const password = ref('admin')
+import { useLocalStorage } from '@vueuse/core'
 
-// const router = useRouter()
+const router = useRouter()
+const loading = ref<any>(false)
+const checked = useLocalStorage('remind_psd', false)
+const formData = useLocalStorage('auth', { username: '', password: ''})
 
 const login = async () => {
-  const res = await api.user.login({ username: username.value, password: password.value })
-  // await store.user.login({ username: username.value, password: password.value })
-  // router.push('/')
-  location.href = '/?token=' + res.token
+  loading.value = true
+  try {
+    const { username, password } = formData.value
+    const res = await api.user.login({ username, password })
+    handleChange()
+    router.push('/?token=' + res.token)
+  } finally {
+    loading.value = false
+  }
+}
+const handleChange = () => {
+  if (!checked.value) {
+    // clear auth data
+    formData.value = {
+      username: '',
+      password: ''
+    }
+  }
 }
 </script>
+<style scoped lang="less">
+.pro-name {
+  position: absolute;
+  left: 40%;
+  top: 40%;
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 60px;
+  font-weight: 600;
+}
+:deep(.ant-layout .ant-layout-content) {
+  padding: 0;
+}
+:deep(body) {
+  background: #f0f2f5;
+}
+.login-container {
+  background: url('../../assets/images/login-bg.png') no-repeat center center;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+  .ant-form {
+    width: 380px;
+    height: 480px;
+    position: absolute;
+    background: #ffffff;
+    border-radius: 4px;
+    right: 230px;
+    top: 50%;
+    transform: translate(0, -50%);
+    padding: 32px 40px 0;
+    .title {
+      font-size: 36px;
+      font-weight: 500;
+      color: #1e2229;
+      line-height: 54px;
+      display: block;
+      margin-bottom: 16px;
+    }
+    .label {
+      margin-bottom: 4px;
+      display: block;
+    }
+  }
+  .ant-checkbox-wrapper {
+    transform: translate(0, -10px);
+  }
+  .ant-input-affix-wrapper {
+    padding: 0 11px 0 0;
+  }
+  .ant-input,
+  .ant-input-affix-wrapper {
+    height: 48px;
+    font-size: 16px;
+    :deep(input) {
+      font-size: 16px;
+      padding: 4px 0 4px 11px;
+    }
+  }
+  .login-btn {
+    width: 100%;
+    height: 48px;
+    font-size: 16px;
+    margin-top: 60px;
+  }
+}
+:deep(input:-webkit-autofill, textarea:-webkit-autofill, select:-webkit-autofill) {
+  box-shadow: inset 0 0 0 1000px #fff;
+  -webkit-box-shadow: 0 0 0 1000px white inset; /* 设置为透明 */
+}
+</style>
