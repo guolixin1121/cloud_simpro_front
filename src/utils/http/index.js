dbc93d4f422aafc4fac4ae2a44e4ca08d6533b5d
Lixin <guolixin1121@126.com>
2023-12-12 10:37:23 +0800

    feat: fix trim bug

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -86,7 +86,9 @@ class AxiosRequest {
         'content-type': type || 'application/json'
       })
       Object.keys(data).forEach(key => {
-        data[key] = data[key]?.trim()
+        if(typeof data[key] === 'string') {
+          data[key] = data[key]?.trim()
+        }
       })
 
       let postData = data
