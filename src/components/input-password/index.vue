20c36a3586843119357e0ca98ed7a2a947549fca
Lixin <guolixin1121@126.com>
2024-08-14 18:22:58 +0800

    fix: input-password trick

--- src/components/input-password/index.vue
+++ src/components/input-password/index.vue
@@ -1,14 +1,17 @@
 <template>
-    <a-input-password maxLength="16" v-model:value="innerValue"  autocomplete="new-password"
+    <a-input-password ref="inputRef" maxLength="16" v-model:value="innerValue"  autocomplete="new-password"
         @change="onChange"></a-input-password>
 </template>
 
 <script lang="ts" setup>
 const props = defineProps(['value']) 
 const innerValue = ref(props.value)
+const inputRef = ref()
 const emits = defineEmits(['update:value'])
 const onChange = (e: any) => {
     innerValue.value = e.target.value.trim()
     emits('update:value', innerValue.value)
+    inputRef.value.blur()
+    inputRef.value.focus()
 }
 </script>
