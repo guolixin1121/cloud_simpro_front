fba0fbb62e4d0a961f83c13c85effcc02ffefc0e
Lixin <guolixin1121@126.com>
2024-08-14 09:13:33 +0800

    fix: bug 31491

---
+++ src/components/input-password/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <a-input-password maxLength="16" v-model:value="innerValue"  autocomplete="new-password"
+        @change="onChange"></a-input-password>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(['value']) 
+const innerValue = ref(props.value)
+const emits = defineEmits(['update:value'])
+const onChange = (e: any) => {
+    innerValue.value = e.target.value.trim()
+    emits('update:value', innerValue.value)
+}
+</script>
--- src/layout/header.vue
+++ src/layout/header.vue
@@ -35,35 +35,13 @@
     <Personal></Personal>
   </a-modal>
   <a-modal title="修改密码" v-model:visible="showChangePassword" :footer="null" destroy-on-close="true">
-    <!-- <Password></Password> -->
-    <a-form ref="formRef" :labelCol="{ style: { width: '75px' } }" :model="formData" style="margin: 24px">
-      <a-form-item name="origin_password" label="原密码" :rules="[
-          { required: true, message: '请填写原密码' },
-          { validator: () => validatePassword(formData.origin_password) } ]">
-        <a-input-password maxLength="16" name="origin_password" autocomplete="new-password" v-model:value="formData.origin_password" placeholder="请填写原密码"></a-input-password>
-      </a-form-item>
-      <a-form-item name="password" label="新密码" :rules="[{ required: true, message: '请填写新密码' },
-        { validator: () => validatePassword(formData.password) }
-      ]">
-        <a-input-password maxLength="16" name="password" v-model:value="formData.password" placeholder="请填写新密码" autocomplete="new-password"></a-input-password>
-      </a-form-item>
-      <a-form-item name="confirm_password" label="确认密码"
-        :rules="[{ required: true, message: '请填写确认密码' }, 
-          { validator: () => comparePassword(formData) }]">
-        <a-input-password maxLength="16" name="confirm_password" v-model:value="formData.confirm_password" placeholder="两次密码必须一致且不能包含空格" autocomplete="new-password"></a-input-password>
-      </a-form-item>
-    </a-form>
-    <div class="modal-buttons">
-      <a-button @click="resetPasswordForm">重置</a-button>
-      <a-button class="login-btn" type="primary" :loading="loading" @click="ok">提交</a-button>
-    </div>
+    <Password></Password>
   </a-modal>
 </template>
 
 <script setup lang="ts">
-import { validatePassword, comparePassword } from '@/utils/validate'
 import Personal from './components/personal.vue'
-// import Password from './components/password.vue'
+import Password from './components/password.vue'
 
 const user = store.user
 const showChangePassword = ref(false)
@@ -80,40 +58,6 @@ const managerAcl = computed(() => {
   return ''
 })
 const memberAcl = user.hasAcl('system:memberManage') ? 'system:memberManage' : ''
-
-const formRef = ref()
-const formData = reactive({
-  origin_password: '',
-  password: '',
-  confirm_password: ''
-})
-const loading = ref(false)
-const ok = () => {
-  formRef.value?.validate().then(async () => {
-    try {
-        loading.value = true
-        const res = await api.auth.changePassword({ 
-          oldPassword: formData.origin_password, 
-          newPassword: formData.password, 
-          confirmPassword: formData.confirm_password
-        })
-        if(!res.code) {
-          message.success('密码修改成功，请重新登录')
-          user.logout()
-        }
-    } finally {
-        loading.value = false
-    }
-  })
-}
-const resetPasswordForm = () => formRef.value.resetFields()
-
-watch(formData, () => {
-  console.log('changed')
-  formData.origin_password = formData.origin_password.trim()
-  formData.password = formData.password.trim()
-  formData.confirm_password = formData.confirm_password.trim()
-})
 </script>
 
 <style scoped lang="less">
