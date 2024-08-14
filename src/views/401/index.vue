edddabb51cb0df08dc1db726f170d1e4b7a06517
Lixin <guolixin1121@126.com>
2024-08-14 18:03:36 +0800

    fix: input issue - 修改test page

--- src/views/401/index.vue
+++ src/views/401/index.vue
@@ -1,16 +1,18 @@
 <template>
     <div>test</div>
-    <a-input v-model:value="name" @input="onChange"></a-input>
+    <a-input v-model:value="name" @input="onChange" ref="inputRef"></a-input>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
 const name = ref<string>('')
-
+const inputRef = ref()
 const onChange = () => {
     if(name.value.length > 4) {
         name.value = name.value.slice(0, 4)
     }
+    inputRef.value.blur()
+    inputRef.value.focus()
 }
 </script>
