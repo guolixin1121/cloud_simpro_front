5c11ea13cecbcc6b7c79373d5419c2de48d967fb
Lixin <guolixin1121@126.com>
2024-10-15 18:03:14 +0800

    fix: frame style

--- src/views/my-sceneset/scene/preview.vue
+++ src/views/my-sceneset/scene/preview.vue
@@ -4,7 +4,7 @@
     <a class="cursor-auto">{{ sceneset?.name }}</a>
     <span>{{ sceneName }}</span>
   </div>
-  <iframe :src="previewUrl" style="margin: -16px; margin-top: 0px; width: calc(100% + 32px); height: calc(100% - 22px)"></iframe>
+  <iframe :src="previewUrl" style="margin: -16px; margin-top: -10px; width: calc(100% + 32px); height: calc(100% - 8px)"></iframe>
 </template>
 
 <script setup lang="ts">
